d20fe08960c2a46c26157c1f3548d0c849850647
NORA-NAO <nolasco.ramos.eda@gmail.com>
2024-07-23 18:38:59 -0600

    navbar5

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: {
-      loader: "akamai",
-      path: "",
+    output: 'export',
+    images:{
+      unoptimized:true,
     },
-    assetPrefix: "./",
-    reactStrictMode: true,
 };
 
 export default nextConfig;
